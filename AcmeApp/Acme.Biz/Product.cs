f0d1910e67ef378cf51be493fd0cc8b465cfaf22
ryanmagoon <mr.magoon5@gmail.com>
2016-08-06 16:59:36 -0400

    Added demo code for declaring and populating a generic list

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -16,28 +16,14 @@ namespace Acme.Biz
         #region Constructors
         public Product()
         {
-            //var colorOptions = new string[4];
-            //colorOptions[0] = "Red";
-            //colorOptions[1] = "Espresso";
-            //colorOptions[2] = "White";
-            //colorOptions[3] = "Navy";
-
-            string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
-
-            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
-
-            colorOptions.SetValue("Blue", 3);
-
-            for (int i = 0; i < colorOptions.Length; i++)
-            {
-                colorOptions[i] = colorOptions[i].ToLower();
-            }
-
-            foreach (var color in colorOptions)
-            {
-                Console.WriteLine($"The color is {color}");
-            }
-            
+            var colorOptions = new List<string>();
+            colorOptions.Add("Red");
+            colorOptions.Add("Espresso");
+            colorOptions.Add("White");
+            colorOptions.Add("Navy");
+            colorOptions.Insert(2, "Purple");
+            colorOptions.Remove("White");
+            Console.WriteLine(colorOptions);
         }
         public Product(int productId,
                         string productName,
