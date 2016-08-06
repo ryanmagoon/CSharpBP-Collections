ca500df198ea33daa603a6d004194200a1cec7d4
ryanmagoon <mr.magoon5@gmail.com>
2016-08-06 18:41:13 -0400

    added code demo for using collection initializers

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -16,13 +16,22 @@ namespace Acme.Biz
         #region Constructors
         public Product()
         {
-            var colorOptions = new List<string>();
-            colorOptions.Add("Red");
-            colorOptions.Add("Espresso");
-            colorOptions.Add("White");
-            colorOptions.Add("Navy");
-            colorOptions.Insert(2, "Purple");
-            colorOptions.Remove("White");
+            //var colorOptions = new List<string>();
+            //colorOptions.Add("Red");
+            //colorOptions.Add("Espresso");
+            //colorOptions.Add("White");
+            //colorOptions.Add("Navy");
+            //colorOptions.Insert(2, "Purple");
+            //colorOptions.Remove("White");
+
+            var colorOptions = new List<string>()
+            {
+                "Red",
+                "Espresso",
+                "White",
+                "Navy"
+            };
+
             Console.WriteLine(colorOptions);
         }
         public Product(int productId,
