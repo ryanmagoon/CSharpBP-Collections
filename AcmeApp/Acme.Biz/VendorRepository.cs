8f2ada62d6fabc4ba4adc846e78cab1481959310
ryanmagoon <mr.magoon5@gmail.com>
2016-08-07 01:09:23 -0400

    added code demo for iterating through a generic dictionary

--- AcmeApp/Acme.Biz/VendorRepository.cs
+++ AcmeApp/Acme.Biz/VendorRepository.cs
@@ -90,17 +90,34 @@ namespace Acme.Biz
                 { "XYZ Inc", new Vendor()
                     { VendorId = 8, CompanyName = "XYZ Inc", Email = "xyz@xyz.com"} }
             };
-            Console.WriteLine(vendors["XYZ Inc"]);
-            if (vendors.ContainsKey("XYZ"))
+
+            foreach (var element in vendors)
             {
-                Console.WriteLine(vendors["XYZ"]);
+                var vendor = element.Value;
+                var key = element.Key;
+                Console.WriteLine($"Key: {key} Value: {vendor}");
             }
 
-            Vendor vendor;
-            if (vendors.TryGetValue("XYZ", out vendor))
-            {
-                Console.WriteLine(vendor);
-            }
+            //foreach (var vendor in vendors.Values)
+            //{
+            //    Console.WriteLine(vendor);
+            //}
+
+            //foreach (var companyName in vendors.Keys)
+            //{
+            //    Console.WriteLine(vendors[companyName]);
+            //}
+            //if (vendors.ContainsKey("XYZ"))
+            //{
+            //    Console.WriteLine(vendors["XYZ"]);
+            //}
+
+            //Vendor vendor;
+            //if (vendors.TryGetValue("XYZ", out vendor))
+            //{
+            //    Console.WriteLine(vendor);
+            //}
+
             return vendors;
         }
     }
